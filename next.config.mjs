d9c0b9fc959ab11aaa7bc70764d5945d0ffff5eb
Aiza Nadeem <an3710828@gmail.com>
2025-12-05 15:26:38 +0500

    my commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -8,6 +14,9 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: __dirname, // now works in ES module
   },
 }
 
